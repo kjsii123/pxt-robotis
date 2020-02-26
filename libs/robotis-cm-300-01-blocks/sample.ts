2bad96684248b969ac3338f11fc30ee56b15c573
kigate <kigate@robotis.com>
2020-02-26 09:19:00 +0900

    no message

--- libs/robotis-cm-300-01-blocks/sample.ts
+++ libs/robotis-cm-300-01-blocks/sample.ts
@@ -104,6 +104,70 @@ enum CM300_Joystick_Button {
     JoystickRight
 }
 
+enum CM300_Gesture {
+    /**
+     * Raised when shaken
+     */
+    //% block=shake
+    Shake = 11,  // ACCELEROMETER_EVT_SHAKE
+    /**
+     * Raised when the device tilts up
+     */
+    //% block="tilt up"
+    TiltUp = 1,  // ACCELEROMETER_EVT_TILT_UP
+    /**
+     * Raised when the device tilts down
+     */
+    //% block="tilt down"
+    TiltDown = 2,  // ACCELEROMETER_EVT_TILT_DOWN
+    /**
+     * Raised when the screen is pointing left
+     */
+    //% block="tilt left"
+    TiltLeft = 3,  // ACCELEROMETER_EVT_TILT_LEFT
+    /**
+     * Raised when the screen is pointing right
+     */
+    //% block="tilt right"
+    TiltRight = 4,  // ACCELEROMETER_EVT_TILT_RIGHT
+    /**
+     * Raised when the screen faces up
+     */
+    //% block="face up"
+    FaceUp = 5,  // ACCELEROMETER_EVT_FACE_UP
+    /**
+     * Raised when the screen is pointing up and the board is horizontal
+     */
+    //% block="face down"
+    FaceDown = 6,  // ACCELEROMETER_EVT_FACE_DOWN
+    /**
+     * Raised when the board is falling!
+     */
+    //% block="free fall"
+    FreeFall = 7,  // ACCELEROMETER_EVT_FREEFALL
+    /**
+     * Raised when a 2G shock is detected
+     */
+    //% block="2g (step)"
+    TwoG = 12,  // ACCELEROMETER_EVT_2G
+    /**
+     * Raised when a 3G shock is detected
+     */
+    //% block="3g"
+    ThreeG = 8,  // ACCELEROMETER_EVT_3G
+    /**
+     * Raised when a 6G shock is detected
+     */
+    //% block="6g"
+    SixG = 9,  // ACCELEROMETER_EVT_6G
+    /**
+     * Raised when a 8G shock is detected
+     */
+    //% block="8g"
+    EightG = 10,  // ACCELEROMETER_EVT_8G
+}
+
+
 //% color=#B4009E weight=900 icon="\uf192" block="Input"
 namespace input {
     /**
@@ -167,7 +231,7 @@ namespace input {
     //% blockId=cm300_isController block="#controller is %controller"
     //% weight=50 blockGap=8
     //% trackArgs=0
-    export function isController(controller: Gesture): boolean {
+    export function isController(controller: CM300_Gesture): boolean {
         // return button->isPressed();
         return true;
     }
